b49060b914a35c992f9d454b16b3fafc67c2a6da
qamar mudhafar <qamarmudhafar@yahoo.com>
2019-12-11 00:45:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+class scooter {
+  constructor(year, color, model) {
+    this.year = year;
+    this.color =  color;
+    this.model = model;
+  }
+
+
+
+class driver {
+  constructor(name, age, experiance) {
+    this.name = name;
+    this.age =  age;
+    this.experiance = experiance;
+  }
+}
+
+class PickupLocation{
+  constructor(address, city){
+  this.address = address;
+  this.city = city;
+}
+}
